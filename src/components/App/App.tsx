f9037ea9aa498cd0c246412ce1129acf23cc8f16
milos <milos030@hotmail.rs>
2020-09-03 12:55:17 +0200

    instalacija osnovnih biblioteka

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import {Container} from 'react-bootstrap';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/popper.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
+import {faHome} from '@fortawesome/free-solid-svg-icons';
 function App() {
   return (
-    <div className="App">
-      App komponenta
-    </div>
+    <Container >
+      <FontAwesomeIcon icon= {faHome} />
+      Home
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
 ReactDOM.render(
   <React.StrictMode>
     <App />
