58a7088e9e47a33efddfb4b3e9b1e75644d3721b
milos <milos030@hotmail.rs>
2020-09-03 16:43:16 +0200

    CategoryAdd to homePage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,20 +1,122 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
-import 'jquery/dist/jquery.js';
-import 'popper.js/dist/popper.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import '@fortawesome/fontawesome-free/css/fontawesome.css' ;
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHome} from '@fortawesome/free-solid-svg-icons';
+import { Container, Card, Col, Row } from 'react-bootstrap';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CategoryType from '../../types/CategoryType';
+import { Redirect, Link } from 'react-router-dom';
+import api, { ApiResponse } from '../../api/api';
 
-function HomePage() {
-  return (
-    <Container >
-     
-     <FontAwesomeIcon icon = {faHome}/>
-      Home
-    </Container>
-  );
+interface HomePageState {
+  isUserLoggedIn: boolean;
+  categories: CategoryType[];
 }
 
-export default HomePage;
+interface CategoryDto {
+  categoryId: number;
+  name: string;
+}
+
+export default class HomePage extends React.Component {
+  state: HomePageState;
+
+  constructor(props: Readonly<{}>) {
+    super(props);
+
+    this.state = {
+      isUserLoggedIn: true,
+      categories: [],
+    };
+  }
+
+  private setUserLoggedInState(state: boolean) {
+    this.setState(Object.assign(this.state, {
+      isUserLoggedIn: state,
+    }));
+  }
+
+  private setCategories(categories: CategoryType[]) {
+    this.setState(Object.assign(this.state, {
+      categories: categories,
+    }));
+  }
+
+  render() {
+    if (this.state.isUserLoggedIn === false) {
+      return (
+        <Redirect to="/user/login/" />
+      );
+    }
+
+    return (
+      <Container>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <FontAwesomeIcon icon={ faHome } /> Home Page
+            </Card.Title>
+
+            <Row>
+              { this.state.categories.map(this.renderSingleCategory) }
+            </Row>
+          </Card.Body>
+        </Card>
+      </Container>
+    );
+  }
+
+  private renderSingleCategory(category: CategoryType) {
+    return (
+      <Col xs="12" sm="6" md="4" lg="3">
+        <Card className="mt-3">
+          <Card.Body>
+            <Card.Title as="p">
+              <strong>
+                { category.name }
+              </strong>
+            </Card.Title>
+            <Link to={ `category/${ category.categoryId }/` }
+                  className="btn btn-sm btn-primary btn-block">
+              Click to open
+            </Link>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+
+  private getCategories() {
+    api('/api/category/', 'get', {})
+    .then((res: ApiResponse) => {
+      if (res.status === 'error' || res.status === 'login') {
+        this.setUserLoggedInState(false);
+        return;
+      }
+
+      this.storeCategoriesIntoTheState(res.data);
+    });
+  }
+
+  private storeCategoriesIntoTheState(apiCategories: CategoryDto[]) {
+    if (!apiCategories || apiCategories.length === 0) {
+      this.setCategories([]);
+      return;
+    }
+
+    const categories: CategoryType[] = apiCategories.map(apiCategory => {
+      return {
+        categoryId: apiCategory.categoryId,
+        name: apiCategory.name,
+      };
+    });
+
+    this.setCategories(categories);
+  }
+
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  componentDidUpdate() {
+    this.getCategories();
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,41 @@
 import React from 'react';
-import {  Switch, Route, HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import HomePage from './components/HomePage/HomePage';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {MainMenu,MainMenuItem} from './components/MainMenu/MainMenu';
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/popper.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import { MainMenu, MainMenuItem } from './components/MainMenu/MainMenu';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import ContactPage from './components/ContactPage/ContactPage';
+import AdministratorLoginPage from './components/AdministratorLoginPage/AdministratorLoginPage';
 import CategoryPage from './components/CategoryPage/CategoryPage';
-import UserLoginpage from './components/UserLoginPage/UserLoginPage';
 
-const menuItems: MainMenuItem[] = [
-  new MainMenuItem("Početna", "/"),
+
+const menuItems = [
+  new MainMenuItem("Home", "/"),
   new MainMenuItem("Contact", "/contact/"),
-  new MainMenuItem("Prijava", "/user/login/"),
-  new MainMenuItem("Category 1", "/category/1/"),
-  new MainMenuItem("Category 22", "/category/22/"),
-  new MainMenuItem("Category 31", "/category/31/"),
-  new MainMenuItem("Category 4", "/category/4/"),
+  new MainMenuItem("Admin login", "/administrator/login/"),
   
 ];
 
-
-
-
-
 ReactDOM.render(
   <React.StrictMode>
-    <MainMenu items= {menuItems}/>
+    <MainMenu items={ menuItems }></MainMenu>
     <HashRouter>
       <Switch>
         <Route exact path="/" component={ HomePage } />
         <Route path="/contact" component={ ContactPage } />
-        <Route path="/user/login" component={ UserLoginpage } />
+        <Route path="/administrator/login" component={ AdministratorLoginPage} />
         <Route path="/category/:cId" component={ CategoryPage } />
       </Switch>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 serviceWorker.unregister();
