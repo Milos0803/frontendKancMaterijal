93ae40787c47b9ac438114b638a5d7dc15ef9fd0
milos <milos030@hotmail.rs>
2020-09-16 19:21:21 +0200

    User Registration add

---
+++ src/components/UserRegistrationPage/UserRegistrationPage.tsx
@@ -0,0 +1,204 @@
+import React from 'react';
+import { Container, Card, Form, Col, Alert, Button, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import api, { ApiResponse } from '../../api/api';
+import { Link } from 'react-router-dom';
+
+interface UserRegistrationPageState {
+    isRegistrationComplete: boolean;
+    message: string;
+    formData: {
+        email: string;
+        password: string;
+        forename: string;
+        surname: string;
+        address: string;
+    };
+}
+
+export default class UserRegistrationPage extends React.Component {
+    state: UserRegistrationPageState;
+
+    constructor(props: Readonly<{}>) {
+        super(props);
+
+        this.state = {
+            isRegistrationComplete: false,
+            message: '',
+            formData: {
+                email: '',
+                password: '',
+                forename: '',
+                surname: '',
+                address: '',
+            },
+        };
+    }
+
+    private setMessage(message: string) {
+        this.setState(Object.assign(this.state, {
+            message: message,
+        }));
+    }
+
+    private setRegistrationCompleteState(state: boolean) {
+        this.setState(Object.assign(this.state, {
+            isRegistrationComplete: state,
+        }));
+    }
+
+    private setFormDataField(fieldName: string, value: any) {
+        const newFormData = Object.assign(this.state.formData, {
+            [fieldName]: value,
+        });
+
+        this.setState(Object.assign(this.state, {
+            formData: newFormData,
+        }));
+    }
+
+    private handleFormInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        let fieldName = '';
+
+        switch (event.target.id) {
+            case 'email': fieldName = 'email'; break;
+            case 'password': fieldName = 'password'; break;
+            case 'forename': fieldName = 'forename'; break;
+            case 'surname': fieldName = 'surname'; break;
+            case 'address': fieldName = 'address'; break;
+        }
+
+        if (fieldName === '') {
+            return;
+        }
+
+        this.setFormDataField(fieldName, event.target.value);
+    }
+
+    render() {
+        return (
+            <Container>
+                <Col md={{ span: 8, offset: 2 }}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>
+                                <FontAwesomeIcon icon={faUserPlus} /> User Registration
+                            </Card.Title>
+
+                            {
+                                this.state.isRegistrationComplete ?
+                                    this.renderRegistrationComplete() :
+                                    this.renderRegistrationForm()
+                            }
+
+                            <Alert variant="danger"
+                                className={this.state.message ? '' : 'd-none'}>
+                                {this.state.message}
+                            </Alert>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Container>
+        );
+    }
+
+    private renderRegistrationComplete() {
+        return (
+            <>
+                <p>The registration has been completed succesfully.</p>
+                <p>
+                    <Link to="/user/login/">Click here</Link> to go to the log in page.
+                </p>
+            </>
+        );
+    }
+
+    private renderRegistrationForm() {
+        return (
+            <Form>
+                <Row>
+                    <Col xs="12" lg="6">
+                        <Form.Group>
+                            <Form.Label htmlFor="email">E-mail:</Form.Label>
+                            <Form.Control type="email" id="email"
+                                value={this.state.formData.email}
+                                onChange={(event) => this.handleFormInputChange(event as any)} />
+                        </Form.Group>
+                    </Col>
+
+                    <Col xs="12" lg="6">
+                        <Form.Group>
+                            <Form.Label htmlFor="password">Password:</Form.Label>
+                            <Form.Control type="password" id="password"
+                                value={this.state.formData.password}
+                                onChange={(event) => this.handleFormInputChange(event as any)} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs="12" lg="6">
+                        <Form.Group>
+                            <Form.Label htmlFor="forename">Forename:</Form.Label>
+                            <Form.Control type="text" id="forename"
+                                value={this.state.formData.forename}
+                                onChange={(event) => this.handleFormInputChange(event as any)} />
+                        </Form.Group>
+                    </Col>
+
+                    <Col xs="12" lg="6">
+                        <Form.Group>
+                            <Form.Label htmlFor="surname">Surname:</Form.Label>
+                            <Form.Control type="text" id="surname"
+                                value={this.state.formData.surname}
+                                onChange={(event) => this.handleFormInputChange(event as any)} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Form.Group>
+                    <Form.Label htmlFor="address">Postal address:</Form.Label>
+                    <Form.Control as="textarea" id="address" rows={6}
+                        value={this.state.formData.address}
+                        onChange={(event) => this.handleFormInputChange(event as any)} />
+                </Form.Group>
+
+                <Form.Group>
+                    <Button variant="primary" block
+                        onClick={() => this.doRegister()}>
+                        <FontAwesomeIcon icon={faUserPlus} /> Register
+                    </Button>
+                </Form.Group>
+            </Form>
+        );
+    }
+
+    private doRegister() {
+        const data = {
+            email: this.state.formData.email,
+            password: this.state.formData.password,
+            forename: this.state.formData.forename,
+            surname: this.state.formData.surname,
+            postalAddress: this.state.formData.address,
+        };
+
+        api('/auth/user/register/', 'post', data)
+            .then((res: ApiResponse) => {
+                if (res.status === 'error') {
+                    this.setMessage('There was an error. Please try again.');
+                    return;
+                }
+
+                if (res.data.statusCode !== undefined) {
+                    switch (res.data.statusCode) {
+                        case -6001: this.setMessage(res.data.message); break;
+                    }
+                    return;
+                }
+
+                this.setMessage('');
+                this.setRegistrationCompleteState(true);
+            });
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -11,17 +11,17 @@ import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 import { MainMenu, MainMenuItem } from './components/MainMenu/MainMenu';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import ContactPage from './components/ContactPage/ContactPage';
-import AdministratorLoginPage from './components/AdministratorLoginPage/AdministratorLoginPage';
-import CategoryPage from './components/CategoryPage/CategoryPage';
 import UserLoginPage from './components/UserLoginPage/UserLoginPage';
-
+import CategoryPage from './components/CategoryPage/CategoryPage';
+import UserRegistrationPage from './components/UserRegistrationPage/UserRegistrationPage';
+import AdminLoginPage from './components/AdministratorLoginPage/AdministratorLoginPage';
 
 const menuItems = [
   new MainMenuItem("Home", "/"),
   new MainMenuItem("Contact", "/contact/"),
-  new MainMenuItem("User login", "/user/login/"),
-  new MainMenuItem("Admin login", "/administrator/login/"),
-  
+  new MainMenuItem("Log in", "/user/login/"),
+  new MainMenuItem("Register", "/user/register/"),
+  new MainMenuItem("ADMINISTRATOR", "/administrator/login/"),
 ];
 
 ReactDOM.render(
@@ -32,13 +32,13 @@ ReactDOM.render(
         <Route exact path="/" component={ HomePage } />
         <Route path="/contact" component={ ContactPage } />
         <Route path="/user/login" component={ UserLoginPage } />
-        <Route path="/administrator/login" component={ AdministratorLoginPage} />
+        <Route path="/user/register" component={ UserRegistrationPage } />
         <Route path="/category/:cId" component={ CategoryPage } />
+        <Route path="/administrator/login" component={ AdminLoginPage } />
       </Switch>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 serviceWorker.unregister();
